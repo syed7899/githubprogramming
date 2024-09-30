229818968ed97f0b4ed32107b7bb05f624e5d8ae
Syed <remindme7899@gmail.com>
2024-10-01 00:29:36 +0530

    first commit

---
+++ src/main/java/com/expert/github/githubprogramming/GithubprogrammingApplication.java
@@ -0,0 +1,13 @@
+package com.expert.github.githubprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubprogrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubprogrammingApplication.class, args);
+	}
+
+}
